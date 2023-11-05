b758e4b66b3a8d6335d0e7395724ce89a40ca220
Rio <126503340+its-rio@users.noreply.github.com>
2023-11-05 18:52:43 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import img1 from './images/joomla.png'
+
+function Logo({width = '100px'}) {
+  return (
+    <div><img src={img1} alt="" /></div>
+  )
+}
+
+export default Logo
